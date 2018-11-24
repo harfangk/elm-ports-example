c11cd44d219eec2f46199f74d581460b27115acb
Bonghyun Kim <bonghyun.d.kim@gmail.com>
2018-11-24 21:35:19 +0900

    Set up individual ports
    Signed-off-by: Bonghyun Kim <bonghyun.d.kim@gmail.com>

--- src/Main.elm
+++ src/Main.elm
@@ -1,61 +1,190 @@
 port module Main exposing (main)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (Html, br, button, div, form, input, label, p, text)
+import Html.Attributes exposing (type_, value)
+import Html.Events exposing (onClick, onInput)
+
+
+
+-- Model
 
 
 type alias Model =
-    { count : Int }
+    { cookies : String
+    , keyForSetItem : String
+    , keyForGetItem : String
+    , keyForRemoveItem : String
+    , value : String
+    , receivedItem : String
+    }
 
 
 type alias Flags =
-    { count : Int }
+    String
+
+
+
+-- Init
 
 
 init : Flags -> ( Model, Cmd Msg )
 init flags =
-    ( { count = flags.count }, Cmd.none )
+    ( { cookies = ""
+      , keyForSetItem = ""
+      , keyForGetItem = ""
+      , keyForRemoveItem = ""
+      , value = ""
+      , receivedItem = ""
+      }
+    , Cmd.none
+    )
+
+
+
+-- Update
 
 
 type Msg
-    = Increment
-    | Decrement
+    = EnteredSetItemKey String
+    | EnteredGetItemKey String
+    | EnteredRemoveItemKey String
+    | EnteredValue String
+    | SetLocalStorageItem
+    | GetLocalStorageItem
+    | RemoveLocalStorageItem
+    | ClearLocalStorage
+    | SetCookie
+    | GetCookies
+    | GotCookies String
+    | GotLocalStorageItem { key : String, value : Maybe String }
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Increment ->
-            let
-                newCount =
-                    model.count + 1
-            in
-            ( { model | count = newCount }, cache newCount )
+        EnteredSetItemKey key ->
+            ( { model | keyForSetItem = key }, Cmd.none )
 
-        Decrement ->
-            let
-                newCount =
-                    model.count - 1
-            in
-            ( { model | count = newCount }, cache newCount )
+        EnteredGetItemKey key ->
+            ( { model | keyForGetItem = key }, Cmd.none )
+
+        EnteredRemoveItemKey key ->
+            ( { model | keyForRemoveItem = key }, Cmd.none )
+
+        EnteredValue value ->
+            ( { model | value = value }, Cmd.none )
+
+        SetLocalStorageItem ->
+            ( model, setLocalStorageItem { key = model.keyForSetItem, value = model.value } )
+
+        GetLocalStorageItem ->
+            ( model, getLocalStorageItem model.keyForGetItem )
+
+        ClearLocalStorage ->
+            ( model, clearLocalStorage () )
+
+        RemoveLocalStorageItem ->
+            ( model, removeLocalStorageItem model.keyForRemoveItem )
+
+        GetCookies ->
+            ( model, getCookies () )
+
+        SetCookie ->
+            ( model, setCookie { key = model.keyForSetItem, value = model.value } )
+
+        GotLocalStorageItem { key, value } ->
+            ( { model | receivedItem = key ++ "=" ++ Maybe.withDefault "null" value }, Cmd.none )
+
+        GotCookies cookies ->
+            ( { model | cookies = cookies }, Cmd.none )
+
+
+
+-- View
 
 
 view : Model -> Html Msg
 view model =
     div []
-        [ button [ onClick Increment ] [ text "+1" ]
-        , div [] [ text <| String.fromInt model.count ]
-        , button [ onClick Decrement ] [ text "-1" ]
+        [ div []
+            [ form []
+                [ label [] [ text "Key: ", input [ onInput EnteredSetItemKey, value model.keyForSetItem ] [] ]
+                , label [] [ text "Value: ", input [ onInput EnteredValue, value model.value ] [] ]
+                , button [ type_ "button", onClick SetLocalStorageItem ] [ text "Set Item" ]
+                , button [ type_ "button", onClick SetCookie ] [ text "Set Cookie" ]
+                ]
+            ]
+        , div []
+            [ form []
+                [ label [] [ text "Key: ", input [ onInput EnteredGetItemKey, value model.keyForGetItem ] [] ]
+                , text <| "LocalStorage Item: " ++ model.receivedItem
+                , br [] []
+                , button [ type_ "button", onClick GetLocalStorageItem ] [ text "Get Item" ]
+                ]
+            ]
+        , div []
+            [ text <| "Cookies: " ++ model.cookies
+            , br [] []
+            , button [ type_ "button", onClick GetCookies ] [ text "Get Cookies" ]
+            ]
+        , div []
+            [ form []
+                [ label [] [ text "Key: ", input [ onInput EnteredRemoveItemKey, value model.keyForRemoveItem ] [] ]
+                , button [ type_ "button", onClick RemoveLocalStorageItem ] [ text "Remove Item" ]
+                , button [ type_ "button", onClick ClearLocalStorage ] [ text "Clear LocalStorage" ]
+                ]
+            ]
         ]
+
+
+
+-- Subscriptions
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
+    Sub.batch
+        [ gotLocalStorageItem GotLocalStorageItem
+        , gotCookies GotCookies
+        ]
 
 
-port cache : Int -> Cmd msg
+
+-- Ports
+-- Local Storage
+
+
+port setLocalStorageItem : { key : String, value : String } -> Cmd msg
+
+
+port getLocalStorageItem : String -> Cmd msg
+
+
+port gotLocalStorageItem : ({ key : String, value : Maybe String } -> msg) -> Sub msg
+
+
+port removeLocalStorageItem : String -> Cmd msg
+
+
+port clearLocalStorage : () -> Cmd msg
+
+
+
+-- Cookies
+
+
+port getCookies : () -> Cmd msg
+
+
+port gotCookies : (String -> msg) -> Sub msg
+
+
+port setCookie : { key : String, value : String } -> Cmd msg
+
+
+
+-- Main
 
 
 main : Program Flags Model Msg
